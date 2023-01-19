80bc34618c3a2482dc27c052b6fd7967f33edbd3
Daniel Suen <danielsuen2007@yahoo.ca>
2023-01-19 17:56:03 -0500

    changed to Hashrouter for compatibility reasons

--- src/index.js
+++ src/index.js
@@ -3,14 +3,14 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 import './index.scss';
 import App from './App';
-import {BrowserRouter} from 'react-router-dom';
+import {HashRouter} from 'react-router-dom';
 import * as serviceWorker from './serviceWorker';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
-      <BrowserRouter>
+      <HashRouter>
         <App />
-      </BrowserRouter> 
+      </HashRouter> 
 );
 
 // ReactDOM.render(<BrowserRouter><App /></BrowserRouter> , document.getElementById("root"));
